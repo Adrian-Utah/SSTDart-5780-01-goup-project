686092675f705083dc73698d97be12ff4c395a8a
Jackson Brough <jacksontbrough@gmail.com>
2026-04-23 15:54:37 -0600

    Receiver stage 1

--- Src/application_rx.c
+++ Src/application_rx.c
@@ -1,18 +1,125 @@
 #include "application_rx.h"
 
+#include <stdio.h>
+
+#include "app_config.h"
+#include "board.h"
 #include "stm32f0xx_hal.h"
 #include "uart.h"
 
+#define RX_TEST_ADC_INPUT_PIN GPIO_PIN_1
+#define RX_TEST_ADC_INPUT_CHANNEL ADC_CHANNEL_1
+
+static ADC_HandleTypeDef hadc;
+
+static void rx_wait_for_confirmation(void)
+{
+    char buffer[8];
+
+    while (1) {
+        uart_write_string("Type y and press Enter to start RX sampling.\r\n");
+        if (uart_read_line(buffer, sizeof(buffer)) == 0u) {
+            continue;
+        }
+
+        if ((buffer[0] == 'y') || (buffer[0] == 'Y')) {
+            uart_write_string("Starting RX sampling.\r\n");
+            return;
+        }
+
+        uart_write_string("RX start cancelled. Waiting again.\r\n");
+    }
+}
+
+static void rx_test_adc_init(void)
+{
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_ADC1_CLK_ENABLE();
+
+    GPIO_InitTypeDef gpio = {0};
+    ADC_ChannelConfTypeDef config = {0};
+
+    gpio.Pin = RX_TEST_ADC_INPUT_PIN;
+    gpio.Mode = GPIO_MODE_ANALOG;
+    gpio.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOA, &gpio);
+
+    hadc.Instance = ADC1;
+    hadc.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
+    hadc.Init.Resolution = ADC_RESOLUTION_12B;
+    hadc.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+    hadc.Init.ScanConvMode = ADC_SCAN_DIRECTION_FORWARD;
+    hadc.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+    hadc.Init.LowPowerAutoWait = DISABLE;
+    hadc.Init.LowPowerAutoPowerOff = DISABLE;
+    hadc.Init.ContinuousConvMode = DISABLE;
+    hadc.Init.DiscontinuousConvMode = DISABLE;
+    hadc.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+    hadc.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+    hadc.Init.DMAContinuousRequests = DISABLE;
+    hadc.Init.Overrun = ADC_OVR_DATA_PRESERVED;
+    HAL_ADC_Init(&hadc);
+
+    config.Channel = RX_TEST_ADC_INPUT_CHANNEL;
+    config.Rank = ADC_RANK_CHANNEL_NUMBER;
+    config.SamplingTime = ADC_SAMPLETIME_239CYCLES_5;
+    HAL_ADC_ConfigChannel(&hadc, &config);
+
+    HAL_ADCEx_Calibration_Start(&hadc);
+}
+
+static uint16_t rx_test_adc_read_once(void)
+{
+    HAL_ADC_Start(&hadc);
+    HAL_ADC_PollForConversion(&hadc, HAL_MAX_DELAY);
+    uint16_t value = (uint16_t)HAL_ADC_GetValue(&hadc);
+    HAL_ADC_Stop(&hadc);
+    return value;
+}
+
 void application_rx_init(void)
 {
+    rx_test_adc_init();
     uart_write_string("PROGRAM STARTED\r\n");
     uart_write_string("MODE: RX APPLICATION\r\n");
-    uart_write_string("OOK receive path is not implemented yet.\r\n");
+    uart_write_string("Sampling PA1 and reporting ADC min/max/span.\r\n");
+    rx_wait_for_confirmation();
 }
 
 void application_rx_run(void)
 {
     while (1) {
-        __WFI();
+        uint16_t min_value = 0x0FFFu;
+        uint16_t max_value = 0u;
+        char buffer[96];
+
+        for (uint32_t i = 0u; i < RX_TEST_ADC_SAMPLES_PER_WINDOW; i++) {
+            uint16_t sample = rx_test_adc_read_once();
+
+            if (sample < min_value) {
+                min_value = sample;
+            }
+            if (sample > max_value) {
+                max_value = sample;
+            }
+        }
+
+        uint16_t span = (uint16_t)(max_value - min_value);
+        uint8_t carrier_present = (span >= RX_TEST_SPAN_THRESHOLD_COUNTS) ? 1u : 0u;
+
+        if (carrier_present != 0u) {
+            board_led_on();
+        } else {
+            board_led_off();
+        }
+
+        snprintf(buffer, sizeof(buffer), "min=%u max=%u span=%u carrier=%u\r\n",
+            (unsigned int)min_value,
+            (unsigned int)max_value,
+            (unsigned int)span,
+            (unsigned int)carrier_present);
+        uart_write_string(buffer);
+
+        HAL_Delay(25);
     }
 }
--- Src/application_tx.c
+++ Src/application_tx.c
@@ -3,6 +3,7 @@
 #include "app_config.h"
 #include "dds.h"
 #include "reflectometry.h"
+#include "stm32f0xx_hal.h"
 #include "uart.h"
 
 void application_tx_init(void)
@@ -19,6 +20,15 @@ void application_tx_init(void)
     reflectometry_init();
     uart_write_string("MODE: CHECK ANTENNA THEN TRANSMIT\r\n");
     reflectometry_prepare_guarded_transmit(0u);
+#elif ACTIVE_TX_MODE == TX_MODE_CARRIER_TEST
+    uart_write_string("MODE: CARRIER TEST\r\n");
+    dds_set_amplitude(OOK_TEST_AMPLITUDE);
+    dds_set_frequency(OOK_TEST_FREQUENCY_HZ);
+    dds_output_off();
+    uart_write_string("TX will alternate carrier ON/OFF in long windows.\r\n");
+#elif ACTIVE_TX_MODE == TX_MODE_UART_HEARTBEAT
+    uart_write_string("MODE: UART HEARTBEAT\r\n");
+    uart_write_string("Writing a UART heartbeat once per second.\r\n");
 #else
     uart_write_string("MODE: BYPASS ANTENNA CHECK\r\n");
     reflectometry_prepare_guarded_transmit(1u);
@@ -33,6 +43,18 @@ void application_tx_run(void)
     }
 #elif ACTIVE_TX_MODE == TX_MODE_CHECK_THEN_TRANSMIT
     reflectometry_run_guarded_transmit(0u);
+#elif ACTIVE_TX_MODE == TX_MODE_CARRIER_TEST
+    while (1) {
+        dds_output_on();
+        HAL_Delay(OOK_CARRIER_TEST_ON_MS);
+        dds_output_off();
+        HAL_Delay(OOK_CARRIER_TEST_OFF_MS);
+    }
+#elif ACTIVE_TX_MODE == TX_MODE_UART_HEARTBEAT
+    while (1) {
+        uart_write_string("UART heartbeat\r\n");
+        HAL_Delay(UART_HEARTBEAT_PERIOD_MS);
+    }
 #else
     reflectometry_run_guarded_transmit(1u);
 #endif
